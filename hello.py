0e818867fe06bee73dbc22babc93344b0560bc02
Bram <badams5@u.rochester.edu>
2013-12-04 14:45:40 -0500

    Comments for the Python backend

--- hello.py
+++ hello.py
@@ -1,21 +1,22 @@
+#Imports. Using Flask and subdependencies. Using SQLAlchemy for the database, and Jinja for template rendering 
 from flask import Flask, jsonify, render_template, request, g
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime, time
 
 
-app = Flask(__name__)
+app = Flask(__name__) #the app initializer
 
 
-app.config.from_object('config')
+app.config.from_object('config') #retrieves db data from the config.py file
 
-db = SQLAlchemy(app)
-@app.route('/')
-def initialize():
-    return render_template("index.html")
+db = SQLAlchemy(app) #database for URStudy
+@app.route('/') #route to the home page (index.html)
+def initialize(): #initialization function for index.html, renders homepage
+    return render_template("index.html") 
 
-@app.route('/checkin', methods=['POST'])
+@app.route('/checkin', methods=['POST']) #renders check in page (the one with the DB) by using arguments given from index.html
 def checkinpage():
-	class_area = request.form.get('what_class')
+	class_area = request.form.get('what_class') #requests data from form
 	class_is = request.form.get('section_chose')
 	how_many = request.form.get('how_many')
 	study_area = request.form.get('library_study')
@@ -23,7 +24,7 @@ def checkinpage():
 	end = request.form.get('time_end')
  	body_notes = request.form.get('notes_section')
 	    
- 	t_start = datetime.strptime(pub_date, "%I:%M%p")
+ 	t_start = datetime.strptime(pub_date, "%I:%M%p") #these lines turn the string representations of time into timestamp objects 
  	t_end = datetime.strptime(end, "%I:%M%p")
  	d_start = datetime.utcnow()
  	the_start = d_start.replace(hour = t_start.hour, minute = t_start.minute)
@@ -31,20 +32,18 @@ def checkinpage():
  	the_end = d_end.replace(hour = t_end.hour, minute = t_end.minute)
  	check = datetime.utcnow()
 
-	post = Post(class_area, class_is, how_many, study_area, the_start, the_end, body_notes)
+	post = Post(class_area, class_is, how_many, study_area, the_start, the_end, body_notes) #finally, adds to database
 	db.session.add(post)
-	db.session.commit()
+	db.session.commit() #commits to db
 
-	noob = None
-	if the_end < check:
-		noob = 'hurrah'
 
 	class_id = class_area + class_is 
 
-	posts = Post.query.filter_by( class_area = class_area )
+	posts = Post.query.filter_by( class_area = class_area ) #only pulls up data for classes relevant to user's query
 
 	'''posts = Post.query.filter(Post.end < datetime.utcnow())'''
 
+	#uncomment this to see debug log
 	'''
 	do_something_wrong()
 	raise
@@ -61,6 +60,7 @@ def checkinpage():
 	 end = end,
 	 body_notes = body_notes
 	 )
+	#the method to load a page if a user doesn't want to add a DB entry, but instead see current ones (work in progress)
 @app.route('/checkin/<class_id>', methods = ['GET'])
 def show_db_data():
 	posts = Post.query.filter_by( class_area = class_area)
@@ -71,7 +71,7 @@ def show_db_data():
 
 
 
-
+#the db model
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     class_area = db.Column(db.String(50))
@@ -84,7 +84,7 @@ class Post(db.Model):
    
 
 
-
+    #initializes the DB
     def __init__(self, class_area, class_is, how_many, study_area, pub_date, end, body_notes):
       
         self.class_area = class_area
@@ -101,6 +101,6 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post %r>' % self.end
 
-
+#runs the program
 if __name__ == '__main__':
 	app.run(debug = True)
