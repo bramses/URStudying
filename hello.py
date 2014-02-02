821947fd162834e253a797224608d25f5bf2a3df
Bram Adams <bradams128@gmail.com>
2014-02-02 15:08:01 -0500

    db

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ from wtforms.validators import Required
 app = Flask(__name__) #the app initializer
 
 
-app.config.from_object('config') #retrieves db data from the config.py file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ecmgmzuwzsbpce:v4vkj5xZPTUuArfJL_AOVSnPyG@ec2-50-17-207-54.compute-1.amazonaws.com:5432/d75imr9f5kmvv0'# #retrieves db data from the config.py file
 
 db = SQLAlchemy(app) #database for URStudy
 
